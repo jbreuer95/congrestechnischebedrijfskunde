0df78a878de668ac390b3a8b2e39854188cae0e9
Jelle <mail@jellebreuer.com>
2015-12-07 21:44:58 +0100

    margins fix

--- resources/views/programma.blade.php
+++ resources/views/programma.blade.php
@@ -22,35 +22,35 @@
                     <p><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;voorzitter Landelijk Overleg Technische Bedrijfskunde</strong></p>
                     <p><strong>10.20&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Themasprekers</strong></p>
                     <ul>
-                        <li>Clusteraccreditaties en het borgen van de kwaliteit, drs. Willem van Raaijen, Partner Hobéon</li>
-                        <li>Roadmap 2015: Leven lang leren en Learning Outcomes in het HBO, drs. Tijs Pijls CINOP Advies en Adviseur NLQF en ECVET</li>
-                        <li>Matching Engineers of the future: de match tussen afgestudeerde Technisch Bedrijfskundigen en wensen van het bedrijfsleven, Michael van Straalen, VNO-NCW en Voorzitter MKB-Nederland</li>
-                        <li>De Engineer van de toekomst, drs. Bert Schroën, Faculteitsdirecteur Bèta Sciences Hogeschool Zuyd</li>
+                        <li style="margin-left: 65px">Clusteraccreditaties en het borgen van de kwaliteit, drs. Willem van Raaijen, Partner Hobéon</li>
+                        <li style="margin-left: 65px">Roadmap 2015: Leven lang leren en Learning Outcomes in het HBO, drs. Tijs Pijls CINOP Advies en Adviseur NLQF en ECVET</li>
+                        <li style="margin-left: 65px">Matching Engineers of the future: de match tussen afgestudeerde Technisch Bedrijfskundigen en wensen van het bedrijfsleven, Michael van Straalen, VNO-NCW en Voorzitter MKB-Nederland</li>
+                        <li style="margin-left: 65px">De Engineer van de toekomst, drs. Bert Schroën, Faculteitsdirecteur Bèta Sciences Hogeschool Zuyd</li>
                     </ul>
                     <p><strong>11.45&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Paneldiscussie, geleid door Marc van Kempen</strong></p>
-                    <p>Panelleden:</p>
+                    <p  style="margin-left: 87px">Panelleden:</p>
                     <ul>
-                        <li>ir. Thjeu Houben, Voorzitter HBO-Domein Engineering</li>
-                        <li>drs. Tijs Pijls, CINOP Advies</li>
-                        <li>drs. Bert Schroën, Faculteitsdirecteur Bèta Sciences Hogeschool Zuyd</li>
-                        <li>drs. Willem van Raaijen, Partner Hobéon</li>
+                        <li  style="margin-left: 65px">ir. Thjeu Houben, Voorzitter HBO-Domein Engineering</li>
+                        <li  style="margin-left: 65px">drs. Tijs Pijls, CINOP Advies</li>
+                        <li  style="margin-left: 65px">drs. Bert Schroën, Faculteitsdirecteur Bèta Sciences Hogeschool Zuyd</li>
+                        <li  style="margin-left: 65px">drs. Willem van Raaijen, Partner Hobéon</li>
                     </ul>
                     <p><strong>12:15&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Lunch</strong></p>
                     <p><strong>13.15&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Eerste break out sessie (45 minuten) , met onderwerpen:</strong></p>
                     <ol>
-                        <li>Hoe groot moet de T van Techniek zijn?</li>
-                        <li>Is commercie hetzelfde als marketing?</li>
-                        <li>Change Management de opvolger van Organisatiekunde?</li>
-                        <li>De BOKS BE – de weg naar succes?</li>
-                        <li>Social Skills in 2025</li>
-                        <li>Is ons afstuderen wat het bedrijfsleven, de NVAO of de student vraagt?</li>
-                        <li>ICT, de techniek voorbij…?</li>
-                        <li>Onderzoeksvaardigheden</li>
-                        <li>Operations Management: nog steeds het hart van TBK?</li>
-                        <li>Ervaar de nieuwe TBK game tijdens de Landelijke Docentendag 2016</li>
-                        <li>Learning Outcomes: de nieuwe leerdoelen i.p.v. beroepsproducten?</li>
-                        <li>Brede Bachelor Engineering en Technische Bedrijfskunde: de ervaringen</li>
-                        <li>De nieuwe deeltijd TBK, stapelen of 4 jarig bachelor programma?</li>
+                        <li  style="margin-left: 65px">Hoe groot moet de T van Techniek zijn?</li>
+                        <li  style="margin-left: 65px">Is commercie hetzelfde als marketing?</li>
+                        <li  style="margin-left: 65px">Change Management de opvolger van Organisatiekunde?</li>
+                        <li  style="margin-left: 65px">De BOKS BE – de weg naar succes?</li>
+                        <li  style="margin-left: 65px">Social Skills in 2025</li>
+                        <li  style="margin-left: 65px">Is ons afstuderen wat het bedrijfsleven, de NVAO of de student vraagt?</li>
+                        <li  style="margin-left: 65px">ICT, de techniek voorbij…?</li>
+                        <li  style="margin-left: 65px">Onderzoeksvaardigheden</li>
+                        <li  style="margin-left: 65px">Operations Management: nog steeds het hart van TBK?</li>
+                        <li  style="margin-left: 65px">Ervaar de nieuwe TBK game tijdens de Landelijke Docentendag 2016</li>
+                        <li  style="margin-left: 65px">Learning Outcomes: de nieuwe leerdoelen i.p.v. beroepsproducten?</li>
+                        <li  style="margin-left: 65px">Brede Bachelor Engineering en Technische Bedrijfskunde: de ervaringen</li>
+                        <li  style="margin-left: 65px">De nieuwe deeltijd TBK, stapelen of 4 jarig bachelor programma?</li>
                     </ol>
                     <p><strong>14.00&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Theepauze</strong></p>
                     <p><strong>14.15&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Tweede break out sessie (45 minuten), zelfde thema’s</strong></p>
