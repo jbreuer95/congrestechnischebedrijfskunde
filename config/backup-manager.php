2643f13e6259d5bb770438e194fa6e7c1eab2eea
Jelle <mail@jellebreuer.com>
2015-12-08 21:07:59 +0100

    backup soort van geinstalleerd

---
+++ config/backup-manager.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+    'local' => [
+        'type' => 'Local',
+        'root' => storage_path('app'),
+    ],
+    's3' => [
+        'type' => 'AwsS3',
+        'key'    => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+        'bucket' => '',
+        'root'   => '',
+    ],
+    'gcs' => [
+        'type' => 'Gcs',
+        'key'    => '',
+        'secret' => '',
+        'bucket' => '',
+        'root'   => '',
+    ],
+    'rackspace' => [
+        'type' => 'Rackspace',
+        'username' => '',
+        'key' => '',
+        'container' => '',
+        'zone' => '',
+        'endpoint' => 'https://identity.api.rackspacecloud.com/v2.0/',
+        'root' => '',
+    ],
+    'dropbox' => [
+        'type' => 'Dropbox',
+        'token' => 'P1adV4PBjsgAAAAAAAAB7SB6QKna-JthBadXaW8LYrO6JjSUWZkIHxM3kLyuyip_',
+        'key' => 'wopo7ubhqiog4kq',
+        'secret' => 'wopo7ubhqiog4kq',
+        'app' => 'SQL Dumps CongresTB',
+        'root' => '/',
+    ],
+    'ftp' => [
+        'type' => 'Ftp',
+        'host' => '',
+        'username' => '',
+        'password' => '',
+        'port' => 21,
+        'passive' => true,
+        'ssl' => true,
+        'timeout' => 30,
+        'root' => '',
+    ],
+    'sftp' => [
+        'type' => 'Sftp',
+        'host' => '',
+        'username' => '',
+        'password' => '',
+        'port' => 21,
+        'timeout' => 10,
+        'privateKey' => '',
+        'root' => '',
+    ],
+];
